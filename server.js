19f627f0f5c5006bd53c10f5fd1d5bcd523eae7a
Mohamed Meeran <148966222+mdmeeran90@users.noreply.github.com>
2025-02-10 21:50:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "https://todolist-mern-chi.vercel.app" }));
+app.use(cors({ origin: "https://todolist-mernproject.vercel.app" }));
 
 // Connecting MongoDB
 mongoose.connect('mongodb+srv://mohamedmeeran2104:root@cluster0.8ie5z.mongodb.net/todo-app')
